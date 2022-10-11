3c3d5453805d1063e6531e3595dc8af6ecc7d00a
Jake-Imanaly <93385501+Jake-Imanaly@users.noreply.github.com>
2022-10-12 01:34:36 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,66 +1,29 @@
-"use strict";
-/*
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "Correct number!";
+"use sctrict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".modal-Close");
+const btnsOpenModal = document.querySelectorAll(".modal-OPEN");
 
-document.querySelector(`.number`).textContent = 13;
-document.querySelector(`.score`).textContent = 10;
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-document.querySelector(".guess").value = 91;
-console.log(document.querySelector(`.guess`).value);
-*/
-let number = Math.trunc(Math.random() * 20 + 1);
-let number2 = Math.trunc(Math.random() * 20 + 1);
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
-let score = 20;
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
 
-document.querySelector(".check").addEventListener(`click`, function () {
-  const guess = Number(document.querySelector(`.guess`).value);
-  console.log(guess, typeof guess);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-  if (!guess) {
-    document.querySelector(".message").textContent = "some shit!";
-  } else if (number === guess) {
-    document.querySelector(".number").textContent = number;
-    document.querySelector(".message").textContent = " You Win!";
-    document.querySelector(`body`).style.backgroundColor = `#60b347`;
-    document.querySelector(".number").style.width = "30rem";
-  } else if (number < guess) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "The number less!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "FUCK YOU!";
-      document.querySelesector(".score").textContent = 0;
-    }
-  } else if (number > guess) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "The number greater!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "FUCK YOU!";
-      document.querySelesector(".score").textContent = 0;
-    }
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
 
-    /* Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the score and secretNumber variables
-3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
-*/
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
   }
-});
-
-document.querySelector(".again").addEventListener("click", function () {
-  const again = Number(document.querySelector(".number").value);
-
-  document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".number").style.width = "15rem";
-
-  document.querySelector(".number").textContent = number = Math.trunc(
-    Math.random() * 20 + 1
-  );
-  document.querySelector(".score").textContent = score = 20;
-  document.querySelector(".number").textContent = "?";
 });
